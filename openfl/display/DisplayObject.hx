535f381b1db627d882d4a1f958c31fbedd4848de
Lea Heitz <lea.heitz@fishingcactus.com>
2016-08-22 11:19:03 +0200

    add the move of the child when his transform is make dirty

--- openfl/display/DisplayObject.hx
+++ openfl/display/DisplayObject.hx
@@ -740,7 +740,9 @@ class DisplayObject extends EventDispatcher implements IBitmapDrawable implement
 	private inline function __setTransformDirty ():Void {
 		
 		if (!__transformDirty) {
-			
+			if (__cachedParent != null) {
+				__cachedParent.__setRenderDirty();
+			}
 			__transformDirty = true;
 			__worldTransformDirty++;
 			
